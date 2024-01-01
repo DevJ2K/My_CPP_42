59b388c7d4a467088e96174a7f8482adc718b30e
DevJ2K <ajavontheopro@gmail.com>
2024-01-01 02:05:54 +0100

    ex00

--- CPP_02/ex00/Fixed.hpp
+++ CPP_02/ex00/Fixed.hpp
@@ -6,12 +6,14 @@
 /*   By: tajavon <tajavon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 19:44:29 by tajavon           #+#    #+#             */
-/*   Updated: 2023/12/30 22:14:03 by tajavon          ###   ########.fr       */
+/*   Updated: 2024/01/01 02:02:46 by tajavon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
+# include <iostream>
+# include "Colors.hpp"
 
 class Fixed
 {
@@ -19,12 +21,14 @@ private:
 	int	_raw;
 	const int	_bitsShift;
 public:
-	Fixed();
-	~Fixed();
+	Fixed(); // Constructeur par défaut
+	Fixed( Fixed const & src ); // Constructeur de recopie
+	~Fixed(); // Destructeur
 
-	// int operator=(const Fixed &);
-	int		getRawBits( void ) const;
-	void	setRawBits( int const raw );
+	Fixed	&operator=(Fixed const & rhs); // Surcharge d'opérateur d'affectation
+
+	int		getRawBits( void ) const; // Accesseur
+	void	setRawBits( int const raw ); // Setteur
 };
 
 #endif
