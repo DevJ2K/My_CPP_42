57cd20cbd3413009531b5187345339db91336325
Theo Ajavon <ajavontheopro@gmail.com>
2024-01-04 09:27:48 +0100

    fin ex00

--- CPP_03/ex00/main.cpp
+++ CPP_03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tajavon <tajavon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 17:00:49 by tajavon           #+#    #+#             */
-/*   Updated: 2024/01/02 20:15:10 by tajavon          ###   ########.fr       */
+/*   Updated: 2024/01/04 09:27:14 by tajavon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,10 @@ int	main(void)
 	clap.attack("Lorem Ipsum");
 
 	for (size_t i = 0; i < 5; i++)
-	{
 		clap.beRepaired(20);
-	}
+
 	for (size_t i = 0; i < 11; i++)
-	{
 		clap.takeDamage(12);
-	}
-
-
-	// for (size_t i = 0; i < 6; i++)
-	// {
-	// 	clap.takeDamage(i + 1);
-	// 	clap.beRepaired(i);
-	// }
-
-
-	// clap.getAttackDamage();
-
-	// clap.beRepaired(100);
-	ClapTrap b = clap;
+	clap.attack("Someone");
+	clap.beRepaired(100);
 }
