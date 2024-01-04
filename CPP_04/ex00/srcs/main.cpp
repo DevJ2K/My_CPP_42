3cbc8cbb8b8c7153df92b4f00326f00723613ab4
Theo Ajavon <ajavontheopro@gmail.com>
2024-01-04 14:55:29 +0100

    ex00 with main done

--- CPP_04/ex00/srcs/main.cpp
+++ CPP_04/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tajavon <tajavon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 12:35:26 by tajavon           #+#    #+#             */
-/*   Updated: 2024/01/04 14:41:16 by tajavon          ###   ########.fr       */
+/*   Updated: 2024/01/04 14:54:44 by tajavon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,76 @@
 #include "Colors.hpp"
 #include <iostream>
 
-int main()
-{
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	// const WrongAnimal* i = new WrongCat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound();
-	j->makeSound();
-	meta->makeSound();
+// int main()
+// {
+// 	const Animal* meta = new Animal();
+// 	const Animal* j = new Dog();
+// 	const Animal* i = new Cat();
+// 	// const WrongAnimal* i = new WrongCat();
+// 	std::cout << j->getType() << " " << std::endl;
+// 	std::cout << i->getType() << " " << std::endl;
+// 	i->makeSound();
+// 	j->makeSound();
+// 	meta->makeSound();
 
-	delete meta;
-	delete j;
-	delete i;
-	return 0;
+// 	delete meta;
+// 	delete j;
+// 	delete i;
+// 	return 0;
+// }
+
+static void	display_big_line( std::string title )
+{
+	size_t line_size = 30;
+
+	std::cout << std::endl;
+
+	for (size_t i = 0; i < (line_size - title.length()) / 2; i++)
+		std::cout << BHWHITE << "=" << RESET;
+
+	std::cout << BHCYAN << title << RESET;
+
+	for (size_t i = 0; i < (line_size - title.length()) / 2; i++)
+		std::cout << BHWHITE << "=" << RESET;
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+	return ;
 }
 
-// int	main(void)
-// {
+static void	display_type( std::string type)
+{
+	std::cout << BWHITE << "I'm a " << RESET << BHMAG << type << RESET << std::endl;
+	return ;
+}
 
-// }
+int main()
+{
+	display_big_line("ANIMAL");
+	const Animal* meta = new Animal();
+	display_type(meta->getType());
+	meta->makeSound();
+	delete meta;
+
+	display_big_line("DOG");
+	const Animal* dog = new Dog();
+	display_type(dog->getType());
+	dog->makeSound();
+	delete dog;
+
+	display_big_line("CAT");
+	const Animal* cat = new Cat();
+	display_type(cat->getType());
+	cat->makeSound();
+	delete cat;
+
+
+	display_big_line("WRONGCAT");
+	const WrongAnimal* wrongCat = new WrongCat();
+	display_type(wrongCat->getType());
+	wrongCat->makeSound();
+	delete wrongCat;
+
+	display_big_line("END");
+	return 0;
+}
