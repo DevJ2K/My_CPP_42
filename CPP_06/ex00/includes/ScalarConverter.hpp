ec02c29185d4cf3de6cbd4a0b78ac27893a2a752
Theo Ajavon <ajavontheopro@gmail.com>
2024-01-22 18:31:42 +0100

    convert char to others

--- CPP_06/ex00/includes/ScalarConverter.hpp
+++ CPP_06/ex00/includes/ScalarConverter.hpp
@@ -6,25 +6,25 @@
 /*   By: tajavon <tajavon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 09:33:33 by tajavon           #+#    #+#             */
-/*   Updated: 2024/01/22 18:05:57 by tajavon          ###   ########.fr       */
+/*   Updated: 2024/01/22 18:23:45 by tajavon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
 
+enum e_type
+{
+	CHAR = 1,
+	INT = 2,
+	FLOAT = 3,
+	DOUBLE = 4,
+	INVALID = 5
+};
+
 class ScalarConverter
 {
 private:
-
-	enum e_type
-	{
-		CHAR = 1,
-		INT = 2,
-		FLOAT = 3,
-		DOUBLE = 4,
-		INVALID = 5
-	};
 
 	ScalarConverter();
 
--- CPP_06/ex00/srcs/ScalarConverter.cpp
+++ CPP_06/ex00/srcs/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: tajavon <tajavon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 09:33:30 by tajavon           #+#    #+#             */
-/*   Updated: 2024/01/22 18:11:21 by tajavon          ###   ########.fr       */
+/*   Updated: 2024/01/22 18:31:08 by tajavon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,37 @@ ScalarConverter::~ScalarConverter()
 {
 }
 
+static e_type	getType( std::string value )
+{
+	if (value.length() == 1 && !std::isdigit(value[0]))
+		return (CHAR);
+	else if (value.length() == 1 && isdigit(value[0]))
+		return (INT);
+	else if (value.length() == 3 && value[0] == 'f' && value[1] == 'f' && value[2] == 'f')
+		return (FLOAT);
+	else if (value.length() == 3 && value[0] == 'f' && value[1] == 'f' && value[2] == 'f')
+		return (DOUBLE);
+	else
+		return (INVALID);
+}
 
 void	ScalarConverter::convert( std::string value )
 {
-	e_type type = CHAR;
+	e_type type = getType(value);
 
 	switch (type)
 	{
 	case CHAR:
-		std::cout << "char: " << static_cast<char>(value[0]) << std::endl;
+		char c;
+		c = static_cast<char>(value[0]);
+		if (c == 0)
+			std::cout << "char: Non displayable" << std::endl;
+		else
+			std::cout << "char: '" << static_cast<char>(value[0]) << "'" << std::endl;
+
 		std::cout << "int: " << static_cast<int>(value[0]) << std::endl;
-		std::cout << "float: " << static_cast<char>(value[0]) << std::endl;
-		std::cout << "double: " << static_cast<char>(value[0]) << std::endl;
+		std::cout << "float: " << static_cast<float>(value[0]) << ".0f"<< std::endl;
+		std::cout << "double: " << static_cast<double>(value[0]) << ".0"<< std::endl;
 		break;
 	case INT:
 		std::cout << "char: " << static_cast<char>(value[0]) << std::endl;
