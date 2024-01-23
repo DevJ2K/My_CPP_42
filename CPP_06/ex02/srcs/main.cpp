8128bca1785c35bd4bb0e429f0bd6ac05b5507f0
theo <ajavontheopro@gmail.com>
2024-01-23 23:13:24 +0100

    end of CPP_06

--- CPP_06/ex02/srcs/main.cpp
+++ CPP_06/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tajavon <tajavon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 17:16:14 by tajavon           #+#    #+#             */
-/*   Updated: 2024/01/23 17:30:20 by tajavon          ###   ########.fr       */
+/*   Updated: 2024/01/23 23:12:43 by tajavon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,25 @@
 #include "Colors.hpp"
 #include <cstdlib>
 #include <exception>
+
+static void display_sub_line(std::string title)
+{
+	size_t line_size = 50;
+
+	std::cout << std::endl;
+
+	for (size_t i = 0; i < (line_size - title.length()) / 2; i++)
+		std::cout << BHWHITE << "=" << RESET;
+
+	std::cout << BHCYAN << title << RESET;
+
+	for (size_t i = 0; i < (line_size - title.length()) / 2; i++)
+		std::cout << BHWHITE << "=" << RESET;
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+	return;
+}
 
 Base* generate(void)
 {
@@ -52,42 +71,22 @@ void identify(Base *p)
 		std::cout << BHRED << "Cannot identify a null pointer." << RESET << std::endl;
 		return ;
 	}
-	try
-	{
-		A* a_ = dynamic_cast<A *>(p);
-		(void)a_;
-		// a_->displayType();
-		std::cout << BHGREEN << "I'm a class [A]" << RESET << std::endl;
-	}
-	catch(std::exception& e)
-	{
+	A* a_ = dynamic_cast<A *>(p);
+	B* b_ = dynamic_cast<B *>(p);
+	C* c_ = dynamic_cast<C *>(p);
+	if (a_)
+		a_->displayType();
+	else
 		std::cout << BHRED << "I'm not a class [A]" << std::endl;
-		std::cerr << e.what() << '\n';
-	}
-	try
-	{
-		B* b_ = dynamic_cast<B *>(p);
-		(void)b_;
-		// b_->displayType();
-		std::cout << BHGREEN << "I'm a class [B]" << RESET << std::endl;
-	}
-	catch(const std::exception& e)
-	{
+	if (b_)
+		b_->displayType();
+	else
 		std::cout << BHRED << "I'm not a class [B]" << std::endl;
-		std::cerr << e.what() << '\n';
-	}
-	try
-	{
-		C* c_ = dynamic_cast<C *>(p);
-		(void)c_;
-		// c_->displayType();
-		std::cout << BHGREEN << "I'm a class [C]" << RESET << std::endl;
-	}
-	catch(const std::exception& e)
-	{
+	if (c_)
+		c_->displayType();
+	else
 		std::cout << BHRED << "I'm not a class [C]" << std::endl;
-		std::cerr << e.what() << '\n';
-	}
+
 }
 
 void identify(Base &p)
@@ -96,43 +95,44 @@ void identify(Base &p)
 	{
 		A a_ = dynamic_cast<A &>(p);
 		a_.displayType();
-		std::cout << BHGREEN << "I'm a class [A]" << std::endl;
 	}
 	catch(const std::exception& e)
 	{
 		std::cout << BHRED << "I'm not a class [A]" << std::endl;
-		std::cerr << e.what() << '\n';
 	}
 	try
 	{
 		B b_ = dynamic_cast<B &>(p);
 		b_.displayType();
-		std::cout << BHGREEN << "I'm a class [B]" << std::endl;
 	}
 	catch(const std::exception& e)
 	{
 		std::cout << BHRED << "I'm not a class [B]" << std::endl;
-		std::cerr << e.what() << '\n';
 	}
 	try
 	{
 		C c_ = dynamic_cast<C &>(p);
 		c_.displayType();
-		std::cout << BHGREEN << "I'm a class [C]" << std::endl;
 	}
 	catch(const std::exception& e)
 	{
 		std::cout << BHRED << "I'm not a class [C]" << std::endl;
-		std::cerr << e.what() << '\n';
 	}
 }
 
 int	main(void)
 {
-	Base* base_random = generate();
-	base_random->displayType();
-	identify(base_random);
-	// identify(*base_random);
 
-	delete base_random;
+	for (size_t i = 0; i < 5; i++)
+	{
+		display_sub_line("Generate Random ");
+		Base* base_random = generate();
+		std::cout << BHWHITE << "Find identity by pointer." << std::endl;
+		identify(base_random);
+		std::cout << BHWHITE << "Find identity by reference." << std::endl;
+		identify(*base_random);
+		delete base_random;
+	}
+
+
 }
