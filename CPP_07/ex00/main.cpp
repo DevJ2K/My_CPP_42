5360983888d87d1f79c8de6a52bc48071b24d7df
Theo Ajavon <ajavontheopro@gmail.com>
2024-01-24 09:47:20 +0100

    Update main ex00 and done

--- CPP_07/ex00/main.cpp
+++ CPP_07/ex00/main.cpp
@@ -6,25 +6,35 @@
 /*   By: tajavon <tajavon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 22:26:39 by tajavon           #+#    #+#             */
-/*   Updated: 2024/01/22 22:27:38 by tajavon          ###   ########.fr       */
+/*   Updated: 2024/01/24 09:46:31 by tajavon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+#include "Colors.hpp"
 #include <iostream>
 
 int main( void ) {
-int a = 2;
-int b = 3;
-::swap( a, b );
-std::cout << "a = " << a << ", b = " << b << std::endl;
-std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-std::string c = "chaine1";
-std::string d = "chaine2";
-::swap(c, d);
-std::cout << "c = " << c << ", d = " << d << std::endl;
-std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-return 0;
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << BHBLACK << "Calling ::swap(c, d)" << RESET << std::endl;
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	std::string str1 = "AAAA";
+	std::string str2 = "BBBB";
+
+	std::cout << str1 << " | " << str2 << std::endl;
+	swap(str1, str2);
+	std::cout << str1 << " | " << str2 << std::endl;
+	return (0);
 }
