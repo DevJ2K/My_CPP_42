c099ee6c33cdf1414c20a9be974a83f143718a8d
theo <ajavontheopro@gmail.com>
2024-01-24 22:34:29 +0100

    using algorithm

--- CPP_08/ex00/easyfind.hpp
+++ CPP_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: tajavon <tajavon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 11:47:37 by tajavon           #+#    #+#             */
-/*   Updated: 2024/01/24 14:46:16 by tajavon          ###   ########.fr       */
+/*   Updated: 2024/01/24 22:34:17 by tajavon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,17 @@
 #include <map>
 #include <list>
 
-// class easyfind
-// {
-// public:
-// 	easyfind(/* args */);
-// };
-
-
 template <typename T>
 typename T::const_iterator	easyfind( T lst, int n )
 {
-	for (typename T::const_iterator it = lst.begin(); it != lst.end(); it++)
-	{
-		if (*it == n)
-			return (it);
-	}
+	typename T::const_iterator iterator = std::find(lst.begin(), lst.end(), n);
+	if (iterator != lst.end())
+		return (iterator);
+	// for (typename T::const_iterator it = lst.begin(); it != lst.end(); it++)
+	// {
+	// 	if (*it == n)
+	// 		return (it);
+	// }
 	throw std::invalid_argument("Element not found");
 }
 
