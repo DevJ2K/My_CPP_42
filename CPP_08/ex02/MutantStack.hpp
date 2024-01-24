77baef8290fc31896aaab0eff087d83587d9bf82
Theo Ajavon <ajavontheopro@gmail.com>
2024-01-24 17:20:42 +0100

    TO DO: Ex02

---
+++ CPP_08/ex02/MutantStack.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tajavon <tajavon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/24 17:16:25 by tajavon           #+#    #+#             */
+/*   Updated: 2024/01/24 17:20:28 by tajavon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <stack>
+// https://cplusplus.com/reference/stack/stack/?kw=stack
+
+
+class MutantStack
+{
+private:
+	/* data */
+public:
+	MutantStack(/* args */);
+	~MutantStack();
+};
+
+
+#endif
---
+++ CPP_08/ex02/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tajavon <tajavon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/24 17:16:03 by tajavon           #+#    #+#             */
+/*   Updated: 2024/01/24 17:16:22 by tajavon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+int main()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+	std::cout << *it << std::endl;
+	++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+}
